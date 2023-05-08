d237a69a3b273b6011f739550f3d6b21e0cb7037
Fabrice Foagoue <foagoue_fabrice@bah.com>
2023-05-07 22:49:20 -0400

    Initial commit

---
+++ test/network_address_monitor_stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NetworkAddressMonitorStack from '../lib/network_address_monitor_stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/network_address_monitor_stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NetworkAddressMonitorStack.NetworkAddressMonitorStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
